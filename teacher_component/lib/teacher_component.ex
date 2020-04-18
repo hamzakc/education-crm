4eab71f136ac2610670ea6a27b192cccb5bf7737
Hamza Khan-Cheema <hamzakc@gmail.com>
2020-04-18 10:55:54 +0300

    Update the teacher component API.
    Removed the login action, so we can move this to another module.
    
    The API now expects a UUID of a teacher to lookup all relative data.
    
    Authorization is a concern that still needs to be thought through, however it is
    not a concern of this module.

--- teacher_component/lib/teacher_component.ex
+++ teacher_component/lib/teacher_component.ex
@@ -1,7 +1,30 @@
 defmodule TeacherComponent do
-	alias TeacherComponent.{Auth, Lessons, Children}
+	@moduledoc """
+	This module provides the Public API called by Teachers.
+  It is assumed that the caller to this API has already been authorized
+  and has full access to the data. The authorization is not a concern of this module!
 
-	defdelegate load(email, password), to: Auth # Move Auth to own component
-	defdelegate lessons(teacher), to: Lessons, as: :all
-	defdelegate children(teacher, lesson \\ nil), to: Children, as: :all
+  All interactions to this API require the UUID of the teacher.
+
+  This simple API will allow for a Teacher to
+	* View their profile
+	* View all the lessons they are assigned to
+  * View all the children they teach, with the ability to filter by lesson
+  """
+	alias TeacherComponent.{Lessons, Children}
+
+	@doc """
+	Retrieves all lessons for teacher given a teacher UUID
+	"""
+	defdelegate lessons(teacher_uuid), to: Lessons, as: :all
+
+	@doc """
+	Retrieves all children taught by a given teacher, with a optional lesson UUID
+	"""
+	defdelegate children(teacher_uuid, lesson_uuid \\ nil), to: Children, as: :all
+
+	@doc """
+	Get profile information for teacher UUID
+	"""
+	defdelegate self(teacher_uuid), to: Teachers, as: :find
 end
