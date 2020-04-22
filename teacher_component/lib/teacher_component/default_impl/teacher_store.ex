b219206178c6162e5daf5e2999bdf6e93c45e35a
Hamza Khan-Cheema <hamzakc@gmail.com>
2020-04-22 22:17:53 +0300

    Added test for find_teacher API

--- teacher_component/lib/teacher_component/default_impl/teacher_store.ex
+++ teacher_component/lib/teacher_component/default_impl/teacher_store.ex
@@ -1,13 +1,16 @@
 defmodule TeacherComponent.DefaultImpl.TeacherStore do
 	alias TeacherComponent.Teacher
 
-  @adapter __MODULE__.Mensia
-
 	@callback find(teacher_id :: integer) :: Teacher.t | nil
-	defdelegate find(teacher_id), to: @adapter
+	def find(teacher_id) do
+		impl().find(teacher_id)
+	end
+
+	def impl() do
+    Application.get_env(:teacher_component, :teacher_store, __MODULE__.Mensia)
+  end
 
 	# Macro so future adapters `use` this module
-	# Stolen from https://github.com/sheharyarn/que/blob/master/lib/que/persistence/persistence.ex#L154
   defmacro __using__(_opts) do
     quote do
       @behaviour unquote(__MODULE__)
--- teacher_component/test/test_helper.exs
+++ teacher_component/test/test_helper.exs
@@ -1,1 +1,11 @@
+Mox.defmock(TeacherComponent.DefaultImpl.MockTeacherStore,
+  for: TeacherComponent.DefaultImpl.TeacherStore
+)
+
+Application.put_env(
+  :teacher_component,
+  :teacher_store,
+  TeacherComponent.DefaultImpl.MockTeacherStore
+)
+
 ExUnit.start()
