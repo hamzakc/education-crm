554926ae58016bd74094a53a8b3e1a783b6c1be5
Hamza Khan-Cheema <hamza@khan-cheema.com>
2020-05-08 11:34:34 +0100

    Updated travis.ci

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,8 @@
-cd ./../teacher_component/
-mix test
-cd ./../lesson_component/
+cd teacher_component/
+echo 'Running tests for teacher component'
+mix deps.get --only test && mix test
+
+cd ../lesson_component/
+echo 'Running tests for lesson component'
+mix deps.get --only test
 mix test
