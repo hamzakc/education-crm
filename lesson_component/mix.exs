03cb45db8a752c1b80664f5f9356c7c7ddae174f
Hamza Khan-Cheema <hamza@khan-cheema.com>
2020-05-08 11:56:44 +0100

    Changed elixir version

--- lesson_component/mix.exs
+++ lesson_component/mix.exs
@@ -5,7 +5,7 @@ defmodule LessonComponent.MixProject do
     [
       app: :lesson_component,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env())
