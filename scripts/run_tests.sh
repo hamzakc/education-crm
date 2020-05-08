ba5ece2ceb90ac7e51d3f3a967287a00ecd8184a
Hamza Khan-Cheema <hamza@khan-cheema.com>
2020-05-08 11:50:53 +0100

    update run_scripts test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,9 +3,11 @@ set -ev
 
 cd teacher_component/
 echo 'Running tests for teacher component'
+mix local.hex --force
 mix deps.get --only test && mix test
 
 cd ../lesson_component/
 echo 'Running tests for lesson component'
+mix local.hex --force
 mix deps.get --only test
 mix test
